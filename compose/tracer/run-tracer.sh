62fcc0abe390505ad9686f4b188331686b12ad2a
Johannes Lund <anviking@me.com>
2025-04-15 10:47:12 +0200

    Combine & write logs to sdk.jsonl

--- compose/tracer/run-tracer.sh
+++ compose/tracer/run-tracer.sh
@@ -5,4 +5,27 @@
 
 chown -R cardano /opt/cardano-tracer
 
-runuser -u cardano -- /usr/local/bin/cardano-tracer $@
+ANTITHESIS_OUTPUT_DIR="${ANTITHESIS_OUTPUT_DIR:-/tmp}"
+
+combine_filtered_logs() {
+
+    # wait for cardano-tracer to create the log files
+    # however, this will cause us to miss the first logs though
+    sleep 2
+
+    for logfile in /opt/cardano-tracer/p*.example_3001/node-*.json; do
+
+	# Filter for traces we care about
+	# Rewrite to format antithesis expects
+	# https://antithesis.com/docs/using_antithesis/sdk/fallback/lifecycle/#event
+	local filter="AdoptedBlock|SwitchedToAFork|ForgedBlock|AddBlockEvent"
+        tail -f "$logfile" 2>/dev/null \
+		| grep --line-buffered -E "Trace(${filter})" \
+		| jq -c --unbuffered '{ (.data.kind | sub("^Trace";"")): {host: .host, data: .data, at: .at} }' \
+		>> $ANTITHESIS_OUTPUT_DIR/sdk.jsonl &
+    done
+}
+
+combine_filtered_logs &
+
+runuser -u cardano -- /usr/local/bin/cardano-tracer "$@"
