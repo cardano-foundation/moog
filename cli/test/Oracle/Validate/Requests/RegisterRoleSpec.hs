cd6388ba92122bce7699e8ea13e139bfea6cbb80
Pawel Jakubas <jakubas.pawel@gmail.com>
2025-09-22 14:59:13 +0200

    make unit pass

--- cli/test/Oracle/Validate/Requests/RegisterRoleSpec.hs
+++ cli/test/Oracle/Validate/Requests/RegisterRoleSpec.hs
@@ -273,7 +273,7 @@ spec = do
                     test =
                         validateUnregisterRole validation forRole
                             $ unregisterRoleChange (Platform platform) user repo
-                pure $ runValidate test `shouldReturn` ValidationSuccess Validated
+                pure $ runValidate test `shouldReturn` ValidationFailure UnregisterRoleRoleIsStillValidInGithub
 
         it
             "fail to validate a unregister role if a request for that key is already pending"
