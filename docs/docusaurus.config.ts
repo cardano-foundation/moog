6363617b6469ab0df57fb6d906dd7328d030a64c
Arnaud Bailly <arnaud@pankzsoft.com>
2025-10-07 08:38:39 +0200

    feat: parameterise site URL for deployment

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/cardano-ada-logo.svg',
 
   // Set the production url of your site here
-  url: 'https://cardano-foundation.github.io/',
+  url: process.env.ANTITHESIS_SITE_URL ?? 'https://cardano-foundation.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/antithesis',
+  baseUrl: process.env.ANTITHESIS_SITE_BASE_URL ?? '/antithesis',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
