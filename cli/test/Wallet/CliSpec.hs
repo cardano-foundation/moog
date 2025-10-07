a81686726c6d95675ca48ac675020278a3721eda
Pawel Jakubas <jakubas.pawel@gmail.com>
2025-10-07 18:21:16 +0200

    add wallet from mnemonic test 1

--- cli/test/Wallet/CliSpec.hs
+++ cli/test/Wallet/CliSpec.hs
@@ -3,27 +3,45 @@ module Wallet.CliSpec
     )
 where
 
+import Core.Types.Mnemonics
+    ( Mnemonics (..)
+    )
+import Core.Types.Wallet (Wallet)
 import Data.Either (isRight)
 import Data.Text (Text)
+import Submitting (readWallet)
 import System.IO.Temp (withSystemTempDirectory)
 import Test.Hspec (Spec, describe, it, shouldReturn, shouldSatisfy)
 import Wallet.Cli
-    ( WalletCommand (Create)
+    ( WalletCommand (Create, Encrypt)
     , WalletError (WalletPresent)
     , walletCmd
     )
 
+import qualified Submitting as Submitting
+
+obtainDecryptedWallet :: Text -> Either Submitting.WalletError Wallet
+obtainDecryptedWallet mnemonicText =
+    let mnemonic = ClearText mnemonicText
+    in readWallet (False, mnemonic)
+
 spec :: Spec
-spec = describe "Wallet create" $ do
-    it "fails if the wallet is already present" $ do
-        withSystemTempDirectory "wallet-cli-spec" $ \dir -> do
-            let wallet = dir <> "/wallet"
-            writeFile wallet ""
-            let command = Create wallet (Nothing :: Maybe Text)
-            walletCmd command `shouldReturn` Left WalletPresent
-    it "succeeds if the wallet is not present" $ do
-        withSystemTempDirectory "wallet-cli-spec" $ \dir -> do
-            let wallet = dir <> "/wallet"
-            let command = Create wallet (Nothing :: Maybe Text)
-            res <- walletCmd command
-            res `shouldSatisfy` isRight
+spec = do
+    describe "wallet create" $ do
+        it "fails if the wallet is already present" $ do
+            withSystemTempDirectory "wallet-cli-spec" $ \dir -> do
+                let wallet = dir <> "/wallet"
+                writeFile wallet ""
+                let command = Create wallet (Nothing :: Maybe Text)
+                walletCmd command `shouldReturn` Left WalletPresent
+        it "succeeds if the wallet is not present" $ do
+            withSystemTempDirectory "wallet-cli-spec" $ \dir -> do
+                let wallet = dir <> "/wallet"
+                let command = Create wallet (Nothing :: Maybe Text)
+                res <- walletCmd command
+                res `shouldSatisfy` isRight
+    describe "wallet encrypt/decrypt" $ do
+        it "wallet can be created from valid mnemonic" $ do
+            let mnemonicText = "culture island clump online fatigue curve fish during mandate echo cradle cat arrange upset region"
+                decryptedWal = obtainDecryptedWallet mnemonicText
+            decryptedWal `shouldSatisfy` isRight
