f3d9b33e44f28188b87940a4a54af0e1dc446e5e
paolino <paolo.veronelli@gmail.com>
2025-12-05 14:18:54 +0000

    CI: run e2e-tests via nix

--- nix/moog-project.nix
+++ nix/moog-project.nix
@@ -69,5 +69,6 @@ in {
     project.hsPkgs.cardano-addresses.components.exes.cardano-address;
   packages.unit-tests = project.hsPkgs.moog.components.tests.unit-tests;
   packages.integration-tests = project.hsPkgs.moog.components.tests.integration-tests;
+  packages.e2e-tests = project.hsPkgs.moog.components.tests.e2e-tests;
   musl64 = project.projectCross.musl64.hsPkgs;
 }
