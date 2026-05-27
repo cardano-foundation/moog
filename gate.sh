df30e96b784418274d3133fb73d7f1f6e53a28c6
Paolo Veronelli <paolo.veronelli@gmail.com>
2026-05-27 12:16:06 +0100

    chore: drop gate.sh (ready for review)

--- gate.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-git diff --check
-nix develop --quiet -c cabal build all --enable-tests
-nix develop --quiet -c cabal test unit-tests --test-show-details=direct
-nix develop --quiet -c cabal-fmt -c moog.cabal CI/rewrite-libs/rewrite-libs.cabal
-nix develop --quiet -c fourmolu -m check src app test CI/rewrite-libs
-nix develop --quiet -c hlint -c src app test CI/rewrite-libs
